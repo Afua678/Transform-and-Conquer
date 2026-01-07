33ae661598a145eb8410d3b794b9c956652b9982
Afua678 <afua.anyadike@gmail.com>
2026-01-07 15:00:26 -0500

    completed idea

--- Gloves.java
+++ Gloves.java
@@ -1,5 +1,4 @@
 public class Gloves {
-    // we probably could implement a two-three tree
-    // actually we could just presort and check if the number in front is the same
+    // presort and check if the number in front is the same
 
 }
