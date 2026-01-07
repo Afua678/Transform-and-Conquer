71ae9152d7c205300b611373c814bdce911579f5
Afua678 <afua.anyadike@gmail.com>
2026-01-07 17:53:26 -0500

    works sometimes

--- Recruitment.java
+++ Recruitment.java
@@ -3,11 +3,14 @@ import java.util.Comparator;
 import java.util.Scanner;
 import java.util.Arrays;
 
+/* Time Complexity: 
+Space Complexity: n [arrays of size n, priority queue takes size n, and variable are constant (1)]
+ */
 public class Recruitment {
 
     // create a class to aid in priority queue
 
-    static class Recruits implements Comparable<Recruits> {
+    static class Recruits { // implements Comparable<Recruits>
         int hires;
         int teachers;
 
@@ -16,13 +19,14 @@ public class Recruitment {
             teachers = b;
         }
 
-        @Override
-        public int compareTo(Recruits other) {
-            if (this.teachers == other.teachers) {
-                return Integer.compare(this.hires, other.hires); // max priority for hires
-            }
-            return Integer.compare(other.teachers, this.teachers); // min priority for teachers
-        }
+        // @Override
+        // public int compareTo(Recruits other) {
+        // if (this.teachers == other.teachers) {
+        // return Integer.compare(other.hires, this.hires); // max priority for hires
+        // }
+        // return Integer.compare(this.teachers, other.teachers); // min priority for
+        // teachers
+        // }
     }
     // do priority queue with teachers as the first priority and hires second
 
@@ -52,12 +56,39 @@ public class Recruitment {
 
         taught = taught.substring(1, taught.length() - 1);
         String[] tp = taught.split(",");
-
-        int[] teaching = new int[tp.length];
+        int[] teaching = new int[tp.length]; // turn into array for teachers
         for (int i = 0; i < tp.length; i++) {
             teaching[i] = Integer.parseInt(tp[i].trim());
         }
 
+        PriorityQueue<Recruits> staff = new PriorityQueue<>(
+                (a, b) -> {
+                    if (a.teachers == b.teachers) { // a.teachers < w && b.teachers < w
+                        return b.hires - a.hires;
+                    }
+                    return a.teachers - b.teachers;
+                });
+        for (int i = 0; i < hiring.length; i++) { // add values from both arrays to queue
+            Recruits b = new Recruits(hiring[i], teaching[i]);
+            staff.add(b);
+        }
+
+        while (w >= 0 && k > 0) {
+            Recruits a = staff.poll();
+            System.out.println(a.hires + " " + a.teachers);
+            if (w - a.teachers >= 0) {
+                w += a.hires;
+                System.out.println(w);
+            }
+            System.out.println(w);
+            k--;
+        }
+
+        if (w <= 0) {
+            System.out.println("The maximum number of teachers is: 0");
+        } else {
+            System.out.println("The maximum number of teachers is: " + w);
+        }
         /*
          * for loop through the end of the array
          * adding
