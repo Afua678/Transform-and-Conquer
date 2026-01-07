59fc935f8944eaa4e0b41b6fc927813a96ef9042
Afua678 <afua.anyadike@gmail.com>
2026-01-07 15:01:50 -0500

    imports

--- Recruitment.java
+++ Recruitment.java
@@ -1,3 +1,7 @@
+import java.util.PriorityQueue;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Recruitment {
     // do priority queue with teachers as the first priority and hires second
 
