0a24b4c6d815ef647651191a392b1526c8d98e9e
Afua678 <afua.anyadike@gmail.com>
2026-01-09 18:56:38 -0500

    finale

--- Recruitment.java
+++ Recruitment.java
@@ -3,8 +3,18 @@ import java.util.Comparator;
 import java.util.Scanner;
 import java.util.Arrays;
 
-/* Time Complexity: O(nlogn + klogn) In the worst case n^2
-Space Complexity: n [arrays of size n, 2 priority queue which takes size n, the number of Recruits Objects is also 0(n), and the numbervariables are constant (1)]
+/* Time Complexity: O(nlogn + klogn)
+Input parsing takes O(n). Building the staff priority queue takes O(nlogn) because it goes through
+n venues with each add to the queue taking logn. Moving the venues from staff to finStaff takes 
+O(nlogn) because each poll and add take log n for each poll and add. The loop selecting up to 
+k venues happens at most k times and each poll takes logn so take (klogn). If k<n, the overall 
+complexity is just nlogn.
+
+
+Space Complexity: O(n)
+[arrays of size n take O(n), 2 priority queues which are O(n), 
+the number of Recruits Objects is also 0(n), 
+and the number of variables are constant O(1)]
  */
 public class Recruitment {
     static class Recruits { // create a class to aid in priority queue
@@ -33,7 +43,7 @@ public class Recruitment {
 
         hired = hired.substring(1, hired.length() - 1);
         String[] temp = hired.split(",");
-        int[] hiring = new int[temp.length]; // turn into array for hires
+        int[] hiring = new int[temp.length]; // turn into array for hires (input parsing)
         for (int i = 0; i < temp.length; i++) {
             hiring[i] = Integer.parseInt(temp[i].trim());
         }
@@ -43,15 +53,15 @@ public class Recruitment {
 
         taught = taught.substring(1, taught.length() - 1);
         String[] tp = taught.split(",");
-        int[] teaching = new int[tp.length]; // turn into array for teachers
+        int[] teaching = new int[tp.length]; // turn into array for teachers (input parsing)
         for (int i = 0; i < tp.length; i++) {
             teaching[i] = Integer.parseInt(tp[i].trim());
         }
 
         PriorityQueue<Recruits> staff = new PriorityQueue<>( // this queue should order by teachers needed
-                                                             // starting with the least, then by # of hires
+                                                             // starting with the least teachers, then by # of hires
                 (a, b) -> {
-                    if (a.teachers == b.teachers) { // if teachers are equal take largest hires
+                    if (a.teachers == b.teachers) { // if teachers are equal, take largest hires
                         return b.hires - a.hires;
                     }
                     return a.teachers - b.teachers;
